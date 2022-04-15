5e808cc01f1ba85736ad4757a66da7dd44a66bde
Wendel Segadilha <wendelsegadilha99@gmail.com>
2022-04-15 11:54:13 -0300

    Criando o Model de Login

--- src/main/java/servlets/ServletLogin.java
+++ src/main/java/servlets/ServletLogin.java
@@ -2,33 +2,45 @@ package servlets;
 
 import java.io.IOException;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
+import model.ModelLogin;
 
 @WebServlet("/ServletLogin")
 public class ServletLogin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
 
-    public ServletLogin() {
-        super();
-    }
+	public ServletLogin() {
+		super();
+	}
 
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
 	}
 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		System.out.println(request.getParameter("nome"));
-		System.out.println(request.getParameter("idade"));
-		
+		String login = request.getParameter("login");
+		String senha = request.getParameter("senha");
+
+		if (login != null && !login.isEmpty() && senha != null && !senha.isEmpty()) {
+
+			ModelLogin modelLogin = new ModelLogin();
+			modelLogin.setLogin(login);
+			modelLogin.setSenha(senha);
+
+		} else {
+			RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
+			request.setAttribute("msg", "Informe o login e senha corretamente!");
+			redirecionar.forward(request, response);
+		}
+
 	}
 
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -8,10 +8,23 @@
 </head>
 <body>
 	<h1>Curso JSP</h1>
+	
 	<form action="ServletLogin" method="post">
-		<input type="text" name="nome">
-		<input type="text" name="idade">
-		<input type="submit" value="Enviar">
+		<table>
+			<tr>
+				<td><label>Login:</label></td>
+				<td><input type="text" name="login"></td>
+			</tr>
+			<tr>
+				<td><label>Senha:</label></td>
+				<td><input type="password" name="senha"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><input type="submit" value="Enviar"></td>
+			</tr>
+		</table>
 	</form>
+	<h4>${msg}</h4>
 </body>
 </html>
